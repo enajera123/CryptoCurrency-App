6be4303b92f002666f50deaf972f3b429e2050dd
enajera123 <estebannajera42@gmail.com>
2023-07-13 13:40:39 -0600

    mas monedas

--- src/data/monedas.js
+++ src/data/monedas.js
@@ -1,8 +1,9 @@
 const monedas = [
   { id: "CRC", nombre: "Colón Costarricense" },
+  { id: "USD", nombre: "Dolar Estadounidense" },
   { id: "MXN", nombre: "Peso Mexicano" },
   { id: "EUR", nombre: "Euro" },
-  { id: "GBP", nombre: "Libra Esterlina" },
+  { id: "GBP", nombre: "Libra Esterlina" }
 ];
 export{
   monedas
